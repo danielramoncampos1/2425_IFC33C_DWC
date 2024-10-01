d7446b744aa517cf11795cdf6adff56348d962fb
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2024-10-01 16:09:34 +0200

    corrección aleatorio entre 1 y 2

--- Cara o cruz/script.js
+++ Cara o cruz/script.js
@@ -3,7 +3,7 @@ document.getElementById("flipButton").addEventListener("click", flipCoin);
 function flipCoin() {
     let resultText = document.getElementById("result");
     let coin = document.getElementById("coin");
-    let result = Math.random() < 0.5 ? "Cara" : "Cruz";
+    let result = Math.floor(Math.random() * 2) + 1 === 1 ? "Cara" : "Cruz";
 
     coin.textContent = "Lanzando...";
     resultText.textContent = "";
