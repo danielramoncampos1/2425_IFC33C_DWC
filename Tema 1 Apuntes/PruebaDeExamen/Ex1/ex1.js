d8668fb829afe3d05f40fbedf4246b451ac3406b
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2024-10-10 16:35:22 +0200

    segunda forma del ejercicio1 de pruebaExamen

--- Tema 1 Apuntes/PruebaDeExamen/Ex1/ex1.js
+++ Tema 1 Apuntes/PruebaDeExamen/Ex1/ex1.js
@@ -1,4 +1,4 @@
-let jsonObj = {
+/*let jsonObj = {
     "squadName": "Super hero squad",
     "homeTown": "Metro City",
     "formed": 2020,
@@ -54,4 +54,57 @@ function displayDetails(jsonObj) {
     });
 }
 
-displayDetails(jsonObj);
+displayDetails(jsonObj);*/
+
+let jsonObj = {
+    squadName: "Super Hero Squad",
+    homeTown: "Metro City",
+    formed: 2020,
+    secretBase: "Super Tower",
+    active: true,
+    members: [
+        {
+            name: "Molecule Man",
+            age: 29,
+            secretIdentity: "Dan Jukes",
+            powers:[
+                "Tadiation resistance", 
+                "Turning Tiny",
+                "Radiation Blast"
+            ]
+        },
+        {
+            name: "Mqdame Uppercut",
+            age: 39,
+            secretIdentity: "Jane Wilson",
+            powers:[
+                "Million tonne punch", 
+                "Damage resistance",
+                "Superhuman reflexes"
+            ]  
+        },
+        {
+            name: "Eternal Flame",
+            age: 1000000,
+            secretIdentity: "Unknown",
+            powers:[
+                "Immortality", 
+                "Heat Immunity",
+                "Inferno",
+                "Teletransportation",
+                "Interdimesional travel"
+            ]
+        }
+    ]
+
+}
+
+console.log(jsonObj.squadName + "\n" + "ciudad de origen: " + jsonObj.homeTown + " // " + "Fecha Origen " + jsonObj.formed + "\n" + 
+    "------------------------------------------------------------------------------------");
+for (let i=0; i<jsonObj.members.length; i++){
+    console.log("Nombre: " + jsonObj.members[i].name + "\n" + 
+                "Edad: " + jsonObj.members[i].age + "\n" + 
+                "Identidad Secreta: " + jsonObj.members[i].secretIdentity + "\n" + 
+                "Poderes: " + jsonObj.members[i].powers.join(', ') + "\n" + 
+                "------------------------------------------------------------------------------------")
+}
