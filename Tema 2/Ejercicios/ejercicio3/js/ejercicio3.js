f97633acddc0b60d20b7d45fbd1f7497a503ebf4
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2024-10-31 16:05:40 +0100

    ejercicio 3 resuelto

--- Tema 2/Ejercicios/ejercicio3/js/ejercicio3.js
+++ Tema 2/Ejercicios/ejercicio3/js/ejercicio3.js
@@ -1,1 +1,19 @@
+/* 
+Ejercicio 3: Generar una lista dinámica
+Crea una lista HTML desde un array de elementos en JavaScript.
+En el archivo JavaScript, crea un array de nombres o elementos.
+Genera una lista <ul> con cada uno de los elementos del array y agrégala al DOM.
+*/
 const nombres = ["Ana", "Luis", "María", "Carlos", "Sofía"];
+
+const body = document.getElementsByTagName("body")[0]
+let ul = document.createElement('ul')
+let li
+
+nombres.forEach(nombre => {
+    li = document.createElement('li');
+    li.textContent = nombre; 
+    ul.appendChild(li); 
+});
+
+body.appendChild(ul)
