b1f6a72c2c4eb1b9cf81fdda2fa3c6f364de15d1
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2025-01-09 16:51:55 +0100

    ejercicio 1 resuelto

---
+++ Tema 3/Ejercicios/3- EjerciciosEventos/Ejercicio1/script.js
@@ -0,0 +1,17 @@
+/* 
+Ejercicio 1: Identificar eventos comunes en una página web
+Visita una página web conocida (por ejemplo, la página de Google) y observa diferentes acciones que puedes realizar, como:
+● Hacer clic en un botón.
+● Escribir texto en una barra de búsqueda.
+● Desplazar la página hacia abajo.
+Anota al menos tres acciones y trata de identificar el tipo de evento que las genera (click, input, scroll, etc.).
+*/
+
+/* 
+
+Botón busca con google, tiene evento click
+
+Usar la rueda del ratón para hacer scroll, tiene evento scroll
+
+Escrbir en la barra de búsqueda, tiene evento input
+*/
