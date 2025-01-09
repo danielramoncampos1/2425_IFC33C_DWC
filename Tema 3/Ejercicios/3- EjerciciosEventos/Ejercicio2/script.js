5a6e203c22bf50fd1fbc7eb4e6e10c915b2548ad
danielramoncampos1 <danielramon@paucasesnovescifp.cat>
2025-01-09 17:18:03 +0100

    ejercicio 2 resuelto

---
+++ Tema 3/Ejercicios/3- EjerciciosEventos/Ejercicio2/script.js
@@ -0,0 +1,15 @@
+/* 
+Ejercicio 2: Agregar un evento básico a un botón
+Escribe un código HTML simple con un botón y utiliza el atributo onclick para mostrar un mensaje de alerta cuando el botón sea presionado.
+Ejemplo inicial:
+<button onclick="alert('¡Has hecho clic!')">Presióname</button>
+Cambia el texto del botón después de hacer clic utilizando una segunda alerta, como:
+alert('El texto del botón cambiará');
+this.innerText = '¡Texto cambiado!’;
+*/
+
+function cambiarTexto(boton) {
+    alert('¡Has hecho clic!');
+    alert('El texto del botón cambiará');
+    boton.innerText = '¡Texto cambiado!';
+}
